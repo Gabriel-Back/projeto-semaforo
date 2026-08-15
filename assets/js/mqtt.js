cd0b26303119a3cc431a865897d603666e9b55a5
Gabriel Schmitk Back <gabriel.back@unifebe.edu.br>
2026-08-15 05:14:13 -0300

    Adicionado docker com o mosquitto, adicionado configuração geral de ws e mqtt e refeito a estilização da página

---
+++ assets/js/mqtt.js
@@ -0,0 +1,44 @@
+=
+
+var pahoConfig = {
+    hostname: "localhost",  
+    port: "9001",           
+    clientId: "teste123"    
+}
+
+client = new Paho.Client(pahoConfig.hostname, Number(pahoConfig.port), pahoConfig.clientId);
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+client.connect({
+    onSuccess: onConnect
+});
+
+function onConnect() {
+    console.log("Connected with Server");
+    client.subscribe("semaforo/estado");
+}
+
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+}
+
+function onMessageArrived(message) {
+    console.log("onMessageArrived:" + message.payloadString);
+    let j = JSON.parse(message.payloadString);
+    handleMessage(j);
+}
+
+function sendMessage(topic, message) {
+    client.publish(topic, message)
+}
+
+function handleMessage(message) {
+    if (message != null && message != undefined) {
+        console.log(message)
+        let mqttMessage = document.getElementById("mqtt-message");
+        mqttMessage.innerHTML = message.msg;
+    }
+}
